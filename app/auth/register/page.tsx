bf48197caa0dc08ee5b5488bb5de8aa2bb0706db
KerdanetYvan <kerdanety@gmail.com>
2025-06-24 14:00:49 +0200

    feat(register): update layout and styling for registration page background elements

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -17,9 +17,9 @@ export default function RegisterPage() {
                 <RegisterForm />
             </div>
             
-            <div className='absolute bottom-0 left-0 w-full h-[300px] overflow-hidden'>
-                <span className='bg-[#FCB259] w-[298px] h-[298px] left-[-50px] bottom-[-150px] rounded-full absolute z-0'></span>
-                <span className='w-[247px] h-[247px] bg-[#FCB259]/40 backdrop-blur-md rounded-full absolute z-10 bottom-[-75px] right-[-50px]'>
+            <div className='absolute bottom-0 left-0 w-full h-[20%] overflow-hidden'>
+                <span className='bg-[#FCB259] w-[298px] h-[298px] left-[-50px] top-[30px] rounded-full absolute z-0'></span>
+                <span className='w-[247px] h-[247px] bg-[#FCB259]/40 backdrop-blur-md rounded-full absolute z-10 top-0 right-[-50px]'>
                 </span>
             </div>
         </div>
