740e686e2289592f656ce4a7780b53cdb21060e1
enzo Couteau <enzocouteau.pro@gmail.com>
2025-06-29 22:15:48 +0200

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-[#454141]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#181B2A] dark:bg-[#23263A]`}
       >
         <AuthProvider>
           {children}
