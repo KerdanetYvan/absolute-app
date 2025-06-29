777e4c512a2171e1545855cdc79e86a3e22295f4
KerdanetYvan <kerdanety@gmail.com>
2025-06-29 03:34:50 +0200

    refactor: remove Test GoBack page and associated link from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -80,15 +80,6 @@ export default function Home() {
               </svg>
               Nous contacter
             </a>
-            <a 
-              href="/test-goback-simple" 
-              className="inline-flex items-center px-6 py-3 bg-green-600 hover:bg-green-700 text-white font-medium rounded-lg transition-colors shadow-lg hover:shadow-xl"
-            >
-              <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-              </svg>
-              Test GoBack
-            </a>
           </div>
         </div>
 
