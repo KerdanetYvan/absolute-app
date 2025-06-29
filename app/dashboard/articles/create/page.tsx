1e6562f418d3781aa6f2f545cc77dbcc695cb52c
KerdanetYvan <kerdanety@gmail.com>
2025-06-29 23:36:04 +0200

    fix(CreateArticlePage): remove unnecessary class names from GoBack component

--- app/dashboard/articles/create/page.tsx
+++ app/dashboard/articles/create/page.tsx
@@ -5,7 +5,7 @@ import NewArticle from '../../../../components/NewArticle'
 export default function CreateArticlePage() {
   return (
     <div className="py-8 max-w-5xl mx-auto">
-      <GoBack className="ml-4 mb-6" />
+      <GoBack />
       <NewArticle onSuccess={() => {}} onError={() => {}} />
     </div>
   );
